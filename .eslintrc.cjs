98054d101598cc4d3bf1b97a2a7eba68da9ea04d
mojahige <mojahige@gmail.com>
2022-09-14 23:05:08 +0900

    add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+  },
+};
